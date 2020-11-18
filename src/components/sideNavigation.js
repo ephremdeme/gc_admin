c7362f530622ca164bdd26e25be14b79ab176144
Ephrem Demelash <demelashephrem@gmail.com>
2020-11-18 23:00:00 +0300

    📦 NEW:  side  navbar

---
+++ src/components/sideNavigation.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import logo from "../assets/mdb-react.png";
+import { MDBListGroup, MDBListGroupItem, MDBIcon } from 'mdbreact';
+import { NavLink } from 'react-router-dom';
+
+const TopNavigation = () => {
+    return (
+        <div className="sidebar-fixed position-fixed">
+            <a href="#!" className="logo-wrapper waves-effect">
+                <img alt="MDB React Logo" className="img-fluid" src={logo}/>
+            </a>
+            <MDBListGroup className="list-group-flush">
+                <NavLink exact={true} to="/" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="chart-pie" className="mr-3"/>
+                        Dashboard
+                    </MDBListGroupItem>
+                </NavLink>
+                <NavLink to="/users" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="users" className="mr-3"/>
+                        Users
+                    </MDBListGroupItem>
+                </NavLink>
+                <NavLink to="/products" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="wallet" className="mr-3"/>
+                        Products
+                    </MDBListGroupItem>
+                </NavLink>
+                <NavLink to="/profile" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="user" className="mr-3"/>
+                        Profile
+                    </MDBListGroupItem>
+                </NavLink>
+                <NavLink to="/tables" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="table" className="mr-3"/>
+                        Tables
+                    </MDBListGroupItem>
+                </NavLink>
+                <NavLink to="/maps" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="map" className="mr-3"/>
+                        Maps
+                    </MDBListGroupItem>
+                </NavLink>
+                <NavLink to="/404" activeClassName="activeClass">
+                    <MDBListGroupItem>
+                        <MDBIcon icon="exclamation" className="mr-3"/>
+                        404
+                    </MDBListGroupItem>
+                </NavLink>
+            </MDBListGroup>
+        </div>
+    );
+}
+
+export default TopNavigation;
