8ae57c7f2b117e9a3c08fc18ebdf8cd30a6cecc1
Ephrem Demelash <demelashephrem@gmail.com>
2020-12-03 21:21:28 +0300

    removes unneccesary routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import CategoriesPage from './category';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CategoriesPage from "./category";
+import SubCategoriesPage from "./category/subCategories";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProfilePage from "./pages/ProfilePage";
-import ProductsPage from './products';
-import UsersPage from './users';
+import ProductsPage from "./products";
+import UsersPage from "./users";
 
 class Routes extends React.Component {
   render() {
@@ -12,13 +13,14 @@ class Routes extends React.Component {
       <Switch>
         {/* <Route path='/' exact component={DashboardPage} />
         <Route path='/dashboard' component={DashboardPage} /> */}
-        <Route path='/profile' component={ProfilePage} />
-        <Route path='/users' component={UsersPage} />
-        <Route path='/products' component={ProductsPage} />
-        <Route path='/categories' component={CategoriesPage} />
+        <Route path="/profile" component={ProfilePage} />
+        <Route path="/users" component={UsersPage} />
+        <Route path="/products" component={ProductsPage} />
+        <Route path="/categories" exact component={CategoriesPage} />
+        <Route path="/categories/:id" component={SubCategoriesPage} />
         {/* <Route path='/tables' component={TablesPage} />
         <Route path='/maps' component={MapsPage} /> */}
-        <Route path='/:id' component={NotFoundPage} />
+        <Route path="/:id" component={NotFoundPage} />
       </Switch>
     );
   }
